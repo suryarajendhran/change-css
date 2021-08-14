759e367da9fa3799e72bf067f061aa53931e9ab3
Surya Rajendhran <cielograndesurya@gmail.com>
2021-08-14 17:57:55 +0530

    create skeleton for modifyCSSRule

---
+++ src/modifyCSSRule.ts
@@ -0,0 +1,9 @@
+import { getCSSRule } from "./getCSSRule";
+
+interface CSSProperties {
+  fontFamily: string,
+}
+
+export function modifyCSSRule(ruleName: string, propertiesToModify: CSSProperties) {
+  console.log('Modify CSS Rule not yet created!');
+}
