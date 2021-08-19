821279c7d1b5bc92cdde3ba428b015793ecb73e3
Surya Rajendhran <cielograndesurya@gmail.com>
2021-08-19 08:43:07 +0530

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  target: 'web',
+  entry: {
+    index: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+    library: 'change-css',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
+  },
+  watchOptions: {
+    aggregateTimeout: 600,
+    ignored: /node_modules/,
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, './dist')],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: [/node_modules/, /test/],
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    // * add some development rules here
+  } else if (argv.mode === 'production') {
+    // * add some prod rules here
+  } else {
+    throw new Error('Specify env');
+  }
+
+  return config;
+};
