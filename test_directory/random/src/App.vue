26a5c23d7268739031cb2268b4263b5b46720631
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2021-08-29 02:56:14 +0530

    Update .gitignore and add test_directory

---
+++ test_directory/random/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { modifyCSSRule } from "change-css";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    modifyCSSRule('#app', {
+      color: 'rgb(128, 128, 128)',
+      fontFamily: 'Helvetica',
+      backgroundColor: 'rgb(230, 230, 230)'
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
