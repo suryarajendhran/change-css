038c483924e30ef6c534429891b9a0612bd63536
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2021-08-29 02:45:59 +0530

    Update tests

--- cypress/integration/default.spec.js
+++ cypress/integration/default.spec.js
@@ -7,8 +7,8 @@ describe('My First Test', () => {
   it('Color is set to grey', () => {
     cy.get('#app').should('have.css', 'color', 'rgb(128, 128, 128)');
   });
-  it('Font is set to Avenir', () => {
-    cy.get('#app').should('have.css', 'font-family', 'Avenir');
+  it('Font is set to Helvetica', () => {
+    cy.get('#app').should('have.css', 'font-family', 'Helvetica');
   });
   it('Background is set to rgb(200, 200, 200)', () => {
     cy.get('#app').should('have.css', 'background-color', 'rgb(230, 230, 230)');
